f330870f26e6dee54caa930ecf6da6c0ded8a8f2
realzhangm <realzhangm@163.com>
2022-04-15 19:47:35 +0800

    compile with kernel 5.13.0-39-generic

--- pid/pid.c
+++ pid/pid.c
@@ -1,8 +1,12 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/init_task.h>
 #include <linux/sched.h>
 #include <linux/list.h>
+
+#define _AUTHOR "zhangm"
+#define _DESC "module_linux"
 
 static __init int print_pid(void)
 {
@@ -11,18 +15,23 @@ static __init int print_pid(void)
 	int count=0;
 	printk("Hello,let begin\n");
 	task=&init_task;
-	list_for_each(pos,&task->tasks)
+	list_for_each(pos, &task->tasks)
 	{
-		p=list_entry(pos,struct task_struct,tasks);
+		p = list_entry(pos, struct task_struct, tasks);
 		count++;
-		printk("%d---->%s\n",p->pid,p->comm);
+		printk("%d---->%s\n", p->pid, p->comm);
 	}
-	printk("the number of process is:%d\n",count);
+	printk("the number of process is:%d\n", count);
 	return 0;
 }
 static __exit void print_exit(void)
 {
-	printk("<0>end!\n");
+	printk("pid end!\n");
 }
+
 module_init(print_pid);
 module_exit(print_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(_AUTHOR);
+MODULE_DESCRIPTION(_DESC);
